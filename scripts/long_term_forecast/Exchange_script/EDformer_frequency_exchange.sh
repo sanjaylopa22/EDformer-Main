a1bccd36be903428ff284d1446f4f0c4fb8272ff
Sanjay Chakraborty <50225655+sanjaylopa22@users.noreply.github.com>
2026-07-06 14:17:52 +0200

    Add files via upload

---
+++ scripts/long_term_forecast/Exchange_script/EDformer_frequency_exchange.sh
@@ -0,0 +1,101 @@
+export CUDA_VISIBLE_DEVICES=4
+
+model_name=EDformer_frequency
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_96_96_learnable \
+  --model EDformer_frequency \
+  --data custom \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp_learnable_alpha' \
+  --itr 1 \
+  --alpha 0.1 \
+  --learnable_alpha 1 \
+  --taper_ratio 0.1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_96_192 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 192 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp_learnable_alpha' \
+  --itr 1 \
+  --alpha 0.1 \
+  --learnable_alpha 1 \
+  --taper_ratio 0.1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_96_336 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 336 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp_learnable_alpha' \
+  --itr 1 \
+  --alpha 0.1 \
+  --learnable_alpha 1 \
+  --taper_ratio 0.1
+  
+  
+ 
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/exchange_rate/ \
+  --data_path exchange_rate.csv \
+  --model_id Exchange_96_720 \
+  --model $model_name \
+  --data custom \
+  --features M \
+  --seq_len 720 \
+  --label_len 48 \
+  --pred_len 96 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 8 \
+  --dec_in 8 \
+  --c_out 8 \
+  --des 'Exp_learnable_alpha' \
+  --itr 1 \
+  --alpha 0.1 \
+  --learnable_alpha 1 \
+  --taper_ratio 0.1
